7f3e9771a2747a4c7530b8d669ad5cb25916001a
jordanc72 <canetejorge30@gmail.com>
2026-04-06 05:29:06 -0300

    Carga de Clases

---
+++ src/ar/com/unpaz/model/Camion.java
@@ -0,0 +1,21 @@
+package ar.com.unpaz.model;
+
+public class Camion extends Vehiculo{
+
+	public Camion(String patente, String marca, double combustible) {
+		super(patente, marca, combustible);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void avanzar(int kilometros) {
+		if(combustible > 0) {
+			
+			double combustibleNecesario = kilometros / 4;
+			this.combustible = Math.max(0, this.combustible - combustibleNecesario);
+		}else {
+			System.out.println("No cuenta con combustible suficiente para avanzar.\n");
+		}
+	}
+
+}
